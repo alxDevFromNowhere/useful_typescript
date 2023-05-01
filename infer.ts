e43ef0b5f78271ed1b25f45622aec152e5b44c50
Alx <131617067+alxDevFromNowhere@users.noreply.github.com>
2023-05-01 14:40:34 +0300

    Update infer.ts

--- infer.ts
+++ infer.ts
@@ -34,3 +34,20 @@ declare function foo(a: string): string;
 declare function foo(a: boolean[]): Array<boolean>
 
 type R = ReturnType<typeof foo>; // boolean[] last call signature
+
+// Use ReturnType
+function getHelloMessage() {
+  return 'Hello from typescript';
+}
+
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : T;
+
+type R = ReturnType<typeof getHelloMessage>
+
+// Use Parameters
+function printversion(version: number) {
+  return `Available new version: ${version}`;
+}
+
+type MyParamType<T> = T extends (...args: infer P) => any ? P : T;
+type R = MyParamType<typeof printversion>[0]
